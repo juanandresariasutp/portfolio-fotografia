a5e168e4530f9632127251fd4c2b93fe8cd834c5
JuanAndres2002 <juanandres.arias@utp.edu.co>
2026-01-25 22:07:07 -0500

    Initial React portfolio setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Mi portafolio fotográfico</h1>
+      <p>Eventos · Deportes · Gastronomía · Retratos</p>
+    </div>
+  );
+}
+
+export default App;
